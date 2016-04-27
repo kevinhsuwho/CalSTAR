907634753afd6e75f5caaa07387aa3a666a32738
kevinhsuwho <kevinhsuwho@gmail.com>
2016-04-26 23:34:15 -0700

    WDD Final
    Open to Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
 
-$(".face .circle").hover(function() {
-    $(this).addClass("circle-hover");
-  }, function() {
-    $(this).removeClass("circle-hover");
-})
+$(".element").typed({
+  strings: ["CALSTAR"],
+  typeSpeed: 350,
+  startDelay: 1000,
+  showCursor: false,
+  callback: showText,
+});
 
+function showText() {
+  $(".picture-text2").fadeIn();
+  $(".picture-text3").fadeIn();
+};
+
+$(".text2").typed({
+  strings: ["3... 2... 1... LIFTOFF!"],
+  typeSpeed: 250,
+  startDelay: 3000,
+  showCursor: false,
+});
+
+$(".face").hover(function() {
+    $(this).find(".circle").addClass("circle-hover");
+  }, function() {
+    $(this).find(".circle").removeClass("circle-hover");
+});
 });
